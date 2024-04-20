9771648327650b5b15ff8ccfcf36efeb4b047951
FabricioIkehara <fah.hiroshi@gmail.com>
2024-04-20 15:46:42 -0300

    banco ok

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Por favor, digite o número da Agência !");
+      int agencia = scanner.nextInt();
+
+      System.out.println("Digite seu número da conta:");
+      int numeroConta = scanner.nextInt();
+      
+      System.out.println("Digite o nome do Usuário da conta:");
+      String nomeusuario = scanner.next();
+
+      System.out.println("Digite o seu saldo:");
+      float saldo = scanner.nextFloat();
+
+      System.out.println("Olá " + nomeusuario + ", obrigado por criar uma conta em nosso banco,55 sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " +  saldo + " já está disponível para saque");
+   }
+ 
+
+    
 }
